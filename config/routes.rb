823b7c304159b6dc15d5f3d29cb235885c951b70
Gabe Smith <gabe@paperlesspost.com>
2012-02-03 16:43:53 -0500

    ajax skeleton!

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,26 @@ get '/' do
   haml :index
 end
 
+# convert sass files on the fly
 get '/css/:file.css' do
   sass :"sass/#{params[:file]}"
 end
 
-get '/load' do
+# Artist, Album, Track Searching
+get '/art/:artist' do
   pass unless request.xhr?
-  sleep 5
-  "Loaded!"
+  sleep 3
+  "> Artist Result!"
+end
+
+get '/alb/:album' do
+  pass unless request.xhr?
+  sleep 1
+  "> Album Result!"
+end
+
+get '/tra/:track' do
+  pass unless request.xhr?
+  sleep 2
+  "> Track Result!"
 end
---
+++ public/js/tchotchke.js
@@ -0,0 +1,51 @@
+function searchAll(e) {
+  var query = $('#search #query').val();
+  artSearch(query);
+  albSearch(query);
+  traSearch(query);
+  e.preventDefault();
+  return false;
+};
+
+function artSearch(artist) {
+  $('#artist_results .result_content').empty();
+  $('#artist_results').show().addClass('loading');
+  $.ajax({
+    url: '/art/'+artist,
+    cache: false,
+    success: function(html){
+      $('#artist_results').removeClass('loading');
+      $('#artist_results .result_content').html(html);
+    }
+  });
+};
+
+function albSearch(album) {
+  $('#album_results .result_content').empty();
+  $('#album_results').show().addClass('loading');
+  $.ajax({
+    url: '/alb/'+album,
+    cache: false,
+    success: function(html){
+      $('#album_results').removeClass('loading');
+      $('#album_results .result_content').html(html);
+    }
+  });
+};
+
+function traSearch(track) {
+  $('#track_results .result_content').empty();
+  $('#track_results').show().addClass('loading');
+  $.ajax({
+    url: '/tra/'+track,
+    cache: false,
+    success: function(html){
+      $('#track_results').removeClass('loading');
+      $('#track_results .result_content').html(html);
+    }
+  });
+};
+
+$(document).ready(function() {
+  $('#search').submit(searchAll);
+});
